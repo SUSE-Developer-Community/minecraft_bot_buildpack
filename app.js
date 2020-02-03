93151f3659b9c1f856d577d2a93ab1fb3cec0b9b
Andrew Gracey <andrew.gracey#suse.com>
2020-02-03 08:46:20 -0800

    switching to user provided service

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const uuidv4 = require('uuid/v4');
 const uuid = uuidv4().split('-')[0];
 const username= `bot-${uuid}`;
 
-const host = process.env.MINECRAFT_SERVER_HOST
-const port = process.env.MINECRAFT_SERVER_PORT
+
+// TODO split out VCAP stuff into helper package
+const exitWithoutService = ()=>{
+  console.error("Programs using this buildpack require a user-provided service: MinecraftServer")
+  console.error("If you are using the SUSE CAP Sandbox, please add the binding using Stratos or `cf bind-service <your app name> MinecraftServer` ")
+  process.exit(127)
+}
+
+const VCAP = process.env.VCAP_SERVICES
+// Verifiy that server is passed in
+if ( !VCAP['user-provided'] || VCAP['user-provided'].length<1 ) {
+  exitWithoutService()
+}
+
+// Verifiy that server is passed in part two
+const server_service = VCAP['user-provided'].find((svc)=>(svc.name==="MinecraftServer"))
+if ( !server_service ) {
+  exitWithoutService()
+}
+
+const { host, port} = server_service.credentials
 
 var bot = mineflayer.createBot({
   username,
