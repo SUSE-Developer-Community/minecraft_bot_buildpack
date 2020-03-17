155c0a0929cd4c46d3bae3d1dcfb544cb7fdf01c
Jeff <jeff.neet@gmail.com>
2020-03-17 05:14:34 -0600

    Added Lewis spawn teleport and giving adventure items

--- lewisBot.js
+++ lewisBot.js
@@ -8,6 +8,8 @@ const navigatePlugin = require('mineflayer-navigate')(mineflayer);
 const host = 'localhost';
 const port = 25565;
 const username = "Lewis_Pugh"
+const spawn = [-384,64,-384]
+var teamName = null;
 
 var bot = mineflayer.createBot({
     username,
@@ -19,18 +21,17 @@ var bot = mineflayer.createBot({
 bot.on('error', err => console.log(err))
 bot.on('respawn',()=>{console.log('respawn')})
 
-// Unique team name only A-Z and _  (no -'s)
-let state = {
-    init: false
-}; // persistant
-
-
 // Sample usage
-console.log(`Initializing ${username}`)
+console.log(`${username}| Initializing`)
 
 // optional configuration
 
 // TODO: Teleport to water and start swimming
+// Teleport to Geeko hideout
+bot.on('login',() => {
+    console.log(`${username}| Login: Teleporting to ocean`)
+    bot.chat(`/tp ${spawn[0]} ${spawn[1]} ${spawn[2]}`)
+})
 // bot.navigate.blocksToAvoid[132] = true; // avoid tripwire
 // bot.navigate.blocksToAvoid[59] = false; // ok to trample crops
 // bot.navigate.on('pathFound', function (path) {
@@ -46,6 +47,52 @@ console.log(`Initializing ${username}`)
 // bot.navigate.on('interrupted', function() {
 //     bot.chat("stopping");
 // });
+function addSpeakerToTeam(chatuser, message) {
+    // parse chat for team
+    console.log('Lewis|Parse for team')
+    var end = message.length - wrapper.TEAM_SUFFIX.length;
+    teamName = message.substring(wrapper.TEAM_PREFIX.length, end);
+    console.log(`Found [${teamName}]`)
+
+    // test for team? - No easy way to test without waiting for chat response
+
+    // make team
+    console.log('Lewis|Make team')
+    bot.chat(`/scoreboard teams add ${teamName}`)
+
+    // set random color?
+    console.log('Lewis|Random Color team?')
+    var colorIndex = Math.floor(Math.random() * wrapper.colors.length - 1);
+    var color = colorIndex < wrapper.colors.length ? wrapper.colors[colorIndex] : wrapper.colors[0]
+    bot.chat(`/scoreboard teams option ${teamName} color ${color}`)
+
+    // join player to team
+    console.log('Lewis|Join team')
+    bot.chat(`/scoreboard teams join ${teamName} ${chatuser}`)
+}
+
+function giveBlocks(chatuser) {
+    bot.chat(`/give @p[name=${chatuser}] grass 64 0 {CanPlaceOn:[sand,sandstone]}`)
+    bot.chat(`/give @p[name=${chatuser}] sapling 64 3 {CanPlaceOn:[grass]}`)
+    bot.chat(`/give @p[name=${chatuser}] dye 64 15 {CanPlaceOn:["sapling"]}`)
+    bot.chat(`/give @p[name=${chatuser}] torch 64 0 {CanPlaceOn:["log",grass,dirt,sand,sandstone]}`)
+}
+
+
+
+bot.on('chat', function(chatuser, message) {
+    // console.log('Lewis|Speaker: ' + chatuser + ', bot username:' + bot.username + '\nmsg: ' + message)
+    // Ignore messages from this bot
+    if (chatuser === bot.username) return;
+
+    // console.log('Lewis|Checking if chat contains ' + wrapper.TEAM_PREFIX + '___' + wrapper.TEAM_SUFFIX)
+
+    if (message.includes(wrapper.TEAM_PREFIX)) {
+        addSpeakerToTeam(chatuser, message);
+        giveBlocks(chatuser);
+    }
+});
+
 bot.on('chat', function(chatuser, message) {
     // console.log('Lewis|Speaker: ' + chatuser + ', bot username:' + bot.username + '\nmsg: ' + message)
     // Ignore messages from this bot
@@ -53,32 +100,8 @@ bot.on('chat', function(chatuser, message) {
 
     console.log('Lewis|Checking if chat contains ' + wrapper.TEAM_PREFIX + '___' + wrapper.TEAM_SUFFIX)
 
-    function addSpeakerToTeam() {
-        // parse chat for team
-        console.log('Lewis|Parse for team')
-        var end = message.length - wrapper.TEAM_SUFFIX.length;
-        var teamName = message.substring(wrapper.TEAM_PREFIX.length, end);
-        console.log(`Found [${teamName}]`)
-
-        // test for team? - No easy way to test without waiting for chat response
-
-        // make team
-        console.log('Lewis|Make team')
-        bot.chat(`/scoreboard teams add ${teamName}`)
-
-        // set random color?
-        console.log('Lewis|Random Color team?')
-        var colorIndex = Math.floor(Math.random() * wrapper.colors.length - 1);
-        var color = colorIndex < wrapper.colors.length ? wrapper.colors[colorIndex] : wrapper.colors[0]
-        bot.chat(`/scoreboard teams option ${teamName} color ${color}`)
-
-        // join player to team
-        console.log('Lewis|Join team')
-        bot.chat(`/scoreboard teams join ${teamName} ${chatuser}`)
-    }
-
-    if (message.includes(wrapper.TEAM_PREFIX)) {
-        addSpeakerToTeam();
+    if (message === wrapper.RESUPPLY_MSG) {
+        giveBlocks(chatuser);
     }
 });
         
