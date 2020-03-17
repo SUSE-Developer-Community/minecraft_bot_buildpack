d9104f113821bddafa3528f386301956fc5bff55
Jeff <jeff.neet@gmail.com>
2020-03-17 03:45:32 -0600

    Create geekoBot.js
    Updated with place holder phrases

---
+++ geekoBot.js
@@ -0,0 +1,41 @@
+/*
+CEO Bot (Standalone)
+Adds new bots to their respective teams upon joining the server
+ */
+var mineflayer = require('mineflayer');
+const wrapper = require('./wrapper.js')(bot)
+const host = 'localhost';
+const port = 25565;
+const username = "Geeko"
+const spawn = [27,64,-110]
+const chatInterval = 15000;
+const phrases = ["Good luck!", "Thanks for saving my forest!", "Say hi to Lewis for me if you see him.", "Watch out for those creepers!", "Bone meal can be applied to saplings for a chance to grow faster.", "You can get bonemeal by defeating skeletons."]
+
+var bot = mineflayer.createBot({
+    username,
+    host,
+    port,
+    version: false
+})
+
+bot.on('error', err => console.log(err))
+bot.on('respawn',()=>{console.log('respawn')})
+
+console.log(`${username}|Initializing`)
+
+// Teleport to Geeko hideout
+bot.on('login',() => {
+    console.log(`${username}| Login: Teleporting to geeko spawn`)
+    bot.chat(`/tp ${spawn[0]} ${spawn[1]} ${spawn[2]}`)
+})
+
+// at set interval, chat random Geeko phrase
+setInterval(()=>{
+    var phraseIndex = Math.floor(Math.random() * phrases.length)
+    bot.chat(phrases[phraseIndex])
+},chatInterval)
+
+//     bot.chat("found path. I can get there in " + path.length + " moves.");
+
+
+        
