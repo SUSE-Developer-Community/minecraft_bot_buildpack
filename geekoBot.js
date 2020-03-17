2b15965c899d91c1e3e8d2b5302e2ff54bdbf9f0
Jeff <jeff.neet@gmail.com>
2020-03-17 05:13:51 -0600

    Phrases update

--- geekoBot.js
+++ geekoBot.js
@@ -9,7 +9,13 @@ const port = 25565;
 const username = "Geeko"
 const spawn = [27,64,-110]
 const chatInterval = 15000;
-const phrases = ["Good luck!", "Thanks for saving my forest!", "Say hi to Lewis for me if you see him.", "Watch out for those creepers!", "Bone meal can be applied to saplings for a chance to grow faster.", "You can get bonemeal by defeating skeletons."]
+const phrases = ["Good luck!",
+    "Thanks for saving my forest!",
+    "Say hi to Lewis for me if you see him.",
+    "Watch out for those creepers!",
+    "Bone meal can be used on saplings for a chance to grow faster.",
+    "You can get bones by defeating skeletons.",
+    "You can craft bonemeal from bones."]
 
 var bot = mineflayer.createBot({
     username,
